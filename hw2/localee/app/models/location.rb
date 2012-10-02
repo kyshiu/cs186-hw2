7cd5a8b2f40434b0ecb23cb9b17b72b568372271
saasbook <saasbook@saasbook.(none)>
2012-10-02 16:18:13 -0700

    location to_hash

--- hw2/localee/app/models/location.rb
+++ hw2/localee/app/models/location.rb
@@ -4,4 +4,12 @@ class Location < ActiveRecord::Base
   has_and_belongs_to_many :users
   has_many :posts
 
+  def to_hash
+    {
+      :id => self.id,
+      :name => self.name,
+      :latitude => self.latitude,
+      :longitude => self.longitude
+    }
+  end
 end
