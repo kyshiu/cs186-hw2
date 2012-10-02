59cca72ba68805cf4cfb4fdc5dbceb16d0abd430
Wesley Prager <wesprager@gmail.com>
2012-10-02 16:42:52 -0700

    Update hw2/localee/db/migrate/20120924055739_users_locations.rb
    lexical ordering

--- hw2/localee/db/migrate/20120924055739_users_locations.rb
+++ hw2/localee/db/migrate/20120924055739_users_locations.rb
@@ -1,6 +1,6 @@
 class UsersLocations < ActiveRecord::Migration
   def change
-    create_table :users_locations, :id => false do |t|
+    create_table :locations_users, :id => false do |t|
       t.integer :user_id
       t.integer :location_id
     end
