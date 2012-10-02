b02410f3d2bb7f8244194cecb0ce3cca7f32b31a
saasbook <saasbook@saasbook.(none)>
2012-10-02 11:37:43 -0700

    associations to user.rb

--- hw2/localee/app/models/user.rb
+++ hw2/localee/app/models/user.rb
@@ -1,21 +1,24 @@
 class User < ActiveRecord::Base
 
+  has_many :posts
+  has_and_belongs_to_many :locations
+
   ################################
   #  DO NOT MODIFY THIS SECTION  #
   ################################
 
   # Use Rails internal password digesting
   has_secure_password
-  
+
   # Validations
   validates :name, :presence => true
   validates :email, :presence => true, :uniqueness => true
   validates :password, :presence => true, :on => :create, :length => {:minimum => 5}
-  
+
   ############################################
   #  TODO: you may optionally add code here  #
   ############################################
-  
+
   def to_hash
     {
       :id => self.id,
