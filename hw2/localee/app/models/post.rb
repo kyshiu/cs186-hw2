81924a47a599b83246489361fcdb7ee751dcb370
Wesley Prager <wesprager@gmail.com>
2012-10-02 17:02:00 -0700

    added to_hash function

--- hw2/localee/app/models/post.rb
+++ hw2/localee/app/models/post.rb
@@ -2,5 +2,13 @@ class Post < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :user
   belongs_to :location
-
+  
+  def to_hash
+    {
+      :id => self.id,
+      :text => self.text,
+      :user_id => self.user_id,
+      :location_id => self.location_id
+    }
+  end
 end
