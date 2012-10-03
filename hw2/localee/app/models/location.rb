74c0f9b665474d4c17df50edfe3a6a1f799706f8
Wesley Prager <wesprager@gmail.com>
2012-10-02 17:13:40 -0700

    added location validations

--- hw2/localee/app/models/location.rb
+++ hw2/localee/app/models/location.rb
@@ -3,6 +3,11 @@ class Location < ActiveRecord::Base
 
   has_and_belongs_to_many :users
   has_many :posts
+  
+  # Validations
+  validates :name, :presence => true
+  validates :latitude, :presence => true
+  validates :longitude, :presence => true
 
   def to_hash
     {
